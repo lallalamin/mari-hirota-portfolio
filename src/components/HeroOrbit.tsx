668e32cc1a0142073da1d48679d8706632124c3d
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-09-21 23:01:41 -0500

    all code added

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,20 +1,53 @@
-import StarIcon from '@/assets/icons/star.svg'
 import { PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export const HeroOrbit = ({children, size, rotation }: PropsWithChildren<{size: number; rotation: number}>) => {
-    return(
-        <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ' >
-            <div className='flex items-start justify-start' style={{
-                transform: `rotate(${rotation}deg)`,
-                height: `${size}px`,
-                width: `${size}px`
-            }}>
-                <div className='inline-flex' style={{
-                    transform: `rotate(${rotation * -1}deg)`,
-                }}>
-                    {children}
-                </div>
+export const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+  orbitDuration,
+  shouldOrbit = false,
+  spinDuration,
+  shouldSpin = false
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  orbitDuration?: string;
+  shouldOrbit?: boolean;
+  spinDuration?: string;
+  shouldSpin?: boolean;
+}>) => {
+  return (
+    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>
+      <div
+        className={twMerge(shouldOrbit && 'animate-spin')}
+        style={{
+          animationDuration: orbitDuration,
+        }}
+      >
+        <div
+          className='flex items-start justify-start '
+          style={{
+            transform: `rotate(${rotation}deg)`,
+            height: `${size}px`,
+            width: `${size}px`,
+          }}
+        >
+          <div className={twMerge(shouldSpin && 'animate-spin')}
+        style={{
+          animationDuration: spinDuration,
+        }}>
+            <div
+              className='inline-flex'
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
             </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,18 +1,26 @@
-export const SectionHeader = ({title, eyebrow, description,} : {title: string; eyebrow: string; description: string;}) => {
+export const SectionHeader = ({
+    title,
+    eyebrow,
+    description
+  }: {
+    title: string;
+    eyebrow: string;
+    description: string;
+  }) => {
     return (
-        <>
-            <div className="flex justify-center">
-                <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">
-                    {title}
-                </p>
-            </div>
-            <h2 className="font-serif text-3xl text-center mt-6 md:text-5xl">
-                {eyebrow}
-            </h2>
-            <p className="text-center text-white/60 mt-4 md.text-lg max-w-md mx-auto lg:text:xl">
-                {description}
-            </p>
-        </>
-        
+      <>
+        <div className='flex justify-center'>
+          <p className='uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400  text-center text-transparent bg-clip-text md:mt-18'>
+            {eyebrow}
+          </p>
+        </div>
+  
+        <h2 className='text-3xl md:text-5xl font-serif text-center mt-6'>
+          {title}
+        </h2>
+        <p className='text-center max-w-md mx-auto md:text-lg lg:text-xl text-white/60 mt-4'>
+          {description}
+        </p>
+      </>
     );
-}
+  };
